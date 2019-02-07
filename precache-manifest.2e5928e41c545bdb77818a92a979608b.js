df61f78ca23ff6dc58f158bc7e263cc2ee143c07
webber2408 <webcrat.tech@gmail.com>
2019-02-07 12:58:56 +0530

    Updates

---
+++ precache-manifest.2e5928e41c545bdb77818a92a979608b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f9431ae127aa9b331a8",
+    "url": "/Food-React/static/js/runtime~main.4f9431ae.js"
+  },
+  {
+    "revision": "b2dde62905a001e7dc98",
+    "url": "/Food-React/static/js/main.b2dde629.chunk.js"
+  },
+  {
+    "revision": "ff49402f4fc0a12071a8",
+    "url": "/Food-React/static/js/1.ff49402f.chunk.js"
+  },
+  {
+    "revision": "b2dde62905a001e7dc98",
+    "url": "/Food-React/static/css/main.0259668c.chunk.css"
+  },
+  {
+    "revision": "4597ea0bda8bbe412f26642e6605934a",
+    "url": "/Food-React/index.html"
+  }
+];
